6fbebdf1ec77edf8184bf814b2ce788419613d27
Randy Tarampi <rt@randytarampi.ca>
2019-04-22 00:31:05 -0700

    Try and let `appium` boot and initialize the emulator on its own.

--- e2e/constants.js
+++ e2e/constants.js
@@ -1,7 +1,10 @@
 import path from 'path';
+import {name} from '../package.json';
 
-export const ANDROID_DEVICE_NAME = process.env.ANDROID_DEVICE_NAME || 'Nexus_5X_API_28';
-export const ANDROID_PLATFORM_VERSION = process.env.ANDROID_PLATFORM_VERSION || '9';
+export const ANDROID_AVD_NAME = process.env.ANDROID_AVD_NAME;
+export const ANDROID_AVD_ARGS = process.env.ANDROID_AVD_ARGS;
+export const ANDROID_DEVICE_NAME = process.env.ANDROID_DEVICE_NAME || ANDROID_AVD_NAME || name;
+export const ANDROID_PLATFORM_VERSION = process.env.ANDROID_PLATFORM_VERSION;
 export const ANDROID_APPLICATION_PATH = process.env.ANDROID_APPLICATION_PATH || path.resolve(__dirname, '../android/app/build/outputs/apk/release/app-release.apk');
 
 export const DEVICE_TIMEOUT = process.env.DEVICE_TIMEOUT || 90 * 1000;
@@ -12,9 +15,9 @@ export const IOS_APPLICATION_PATH = process.env.IOS_APPLICATION_PATH; // FIXME-R
 
 export const APPIUM_HOST = process.env.APPIUM_HOST || 'localhost';
 export const APPIUM_LOG_LEVEL = process.env.APPIUM_LOG_LEVEL || 'debug';
-export const APPIUM_PASSWORD = process.env.APPIUM_PASSWORD || undefined;
+export const APPIUM_PASSWORD = process.env.APPIUM_PASSWORD;
 export const APPIUM_PORT = process.env.APPIUM_PORT && Number(process.env.APPIUM_PORT) || 4723;
-export const APPIUM_USER = process.env.APPIUM_USER || undefined;
+export const APPIUM_USER = process.env.APPIUM_USER;
 
 export const TARGET_PLATFORM = process.env.TARGET_PLATFORM || 'android';
 
